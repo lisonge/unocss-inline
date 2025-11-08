6b65a6d894a6b099f1696378a8c6e76ee22090fa
二刺螈 <i@songe.li>
2025-11-08 22:57:58 +0800

    fix: ensure document body is available before appending iframe for unocss style

--- packages/unocss-inline/src/style.ts
+++ packages/unocss-inline/src/style.ts
@@ -3,15 +3,19 @@ unoStyle.textContent = '/*__UNOCSS_INLINE_STYLE_PLACEHOLDER__*/';
 export default unoStyle;
 
 if (import.meta.hot) {
-  const iframe = document.body.appendChild(document.createElement('iframe'));
-  iframe.setAttribute('data-vite-dev-id', 'unocss-inline/style');
-  iframe.style.display = 'none';
-  const subDocument = iframe.contentDocument!;
-  const script = subDocument.createElement('script');
-  script.type = 'module';
-  script.src = new URL('/__uno.css', import.meta['url']).href; // compat https://github.com/lisonge/vite-plugin-monkey
-  subDocument.head.appendChild(script);
   (async () => {
+    while (!document.body) {
+      // document.body may be null when tampermonkey run-at document-start
+      await new Promise((r) => setTimeout(r));
+    }
+    const iframe = document.body.appendChild(document.createElement('iframe'));
+    iframe.setAttribute('data-vite-dev-id', 'unocss-inline/style');
+    iframe.style.display = 'none';
+    const subDocument = iframe.contentDocument!;
+    const script = subDocument.createElement('script');
+    script.type = 'module';
+    script.src = new URL('/__uno.css', import.meta['url']).href; // compat https://github.com/lisonge/vite-plugin-monkey
+    subDocument.head.appendChild(script);
     const unocssStyle = await (async () => {
       const t = Date.now() + 10_000;
       while (Date.now() < t) {
